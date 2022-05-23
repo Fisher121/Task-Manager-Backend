f6ca8c7f83cfa77f92d6698626afd7990464e08d
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2022-05-24 01:43:39 +0300

    Update Database.js

--- DAO/Database.js
+++ DAO/Database.js
@@ -4,7 +4,7 @@ class Database{
     constructor(){
         this.pool = mysql.createPool({
             host:'34.116.233.107',
-            socketPath:'/cloudsql/taskmanagerbackend:europe-central2:taskmanagerinstance1', // comment this when running locally
+            socketPath:'../cloudsql/taskmanagerbackend:europe-central2:taskmanagerinstance1', // comment this when running locally
             database: 'TaskDB',
             user:'root',
             password:'1234',
